d89b8936393ad5cca69f80540524c0e9e73c3302
Pooja <poojajmpatil210@gmail.com>
2020-02-19 02:00:35 -0800

    sample

--- src/main/java/com/example/demo/JenkinDemoApplication.java
+++ src/main/java/com/example/demo/JenkinDemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinDemoApplication {
+public class JenkinDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinDemoApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinDemoApplication.class);
 	}
 
 }
